9ca2b2b18db6d3d056185ccaa85d0872c4b9279b
Girts Svitins <girts.svitins@gmail.com>
2015-09-06 21:24:36 +0000

    install script update

---
+++ .vim/.vim
@@ -0,0 +1,1 @@
+/home/vagrant/git/dotfiles/.vim
--- install.sh
+++ install.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
 
-ln -s .vim ~/.vim
-ln -s .vimrc ~/.vimrc
-ln -s .bashrc ~/.bashrc
+# Check if git is installed
+if [ -x "/usr/bin/git" ]; then
+    echo "Installing dotfiles..."
+else
+    echo "You need to install git in order to continue with dotfiles installation"
+fi
+
+# Create symbolic links
+ln -sf $PWD/.vim ~/.vim
+ln -sf $PWD/.vimrc ~/.vimrc
+ln -sf $PWD/.bashrc ~/.bashrc
+
+# Install Vundle for vim
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+echo "Installation complete!"
+echo "Please start vim and run ':PluginInstall' command to install all plugins"
 
 
