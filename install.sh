9c0f49093dcfc9d9d42e4855233fec98bed19ddd
Girts Svitins <girts.svitins@gmail.com>
2015-10-03 15:24:13 +0300

    improved install.sh script

--- install.sh
+++ install.sh
@@ -1,23 +1,28 @@
 #!/usr/bin/env bash
 
 # Check if git is installed
-if [ -x "/usr/bin/git" ]; then
-    echo "Installing dotfiles..."
-else
+if [ ! -x "/usr/bin/git" ]; then
     echo "You need to install git in order to continue with dotfiles installation"
 fi
 
-# Create symbolic links
+echo -n "Deleting already deployed Vundle plugins..."
+rm -rf .vim/bundle/*
+echo "done!"
+
+echo -n "Creating symbolic links..."
 ln -sf $PWD/.vim ~/.vim
 ln -sf $PWD/.vimrc ~/.vimrc
 ln -sf $PWD/.bashrc ~/.bashrc
 ln -sf $PWD/.tmux.conf ~/.tmux.conf
-ln -sf $PWD/.tmux/layout ~/.tmux/layout
+ln -sf $PWD/.tmux ~/.tmux
+echo "done!"
 
-# Install Vundle for vim
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+echo -n "Install Vundle (vim plugin manager)..."
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim &> /dev/null
+echo "done!"
+
+vim -c "PluginInstall"
 
 echo "Installation complete!"
-echo "Please start vim and run ':PluginInstall' command to install all plugins"
 
 
